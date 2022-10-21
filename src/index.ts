4c4662db5e8efebc3cf0adc82a7e85710de68a5e
Ashutosh Ukey <ashutosh.ukey@circle.com>
2022-10-21 13:49:28 -0400

    chore: remove leading slash

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./generated";
 
 export enum CircleEnvironments {
-  production = "https://api.circle.com/",
+  production = "https://api.circle.com",
   sandbox = "https://api-sandbox.circle.com"
 }
