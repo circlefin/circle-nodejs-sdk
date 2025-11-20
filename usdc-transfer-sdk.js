c41cdbdec592048ddf9d0c79af9d1195b8e0eac8
Nicholas B <nbzeman@gmail.com>
2025-11-20 17:29:15 +0000

    sdk and api updates

--- usdc-transfer-sdk.js
+++ usdc-transfer-sdk.js
@@ -50,17 +50,32 @@ async function sendUsdcTransfer() {
         console.log(JSON.stringify(transferPayload));
         // 3. Execute the transfer API call
         const response = await circleClient.createTransaction(transferPayload);
-        // const response = await circleClient.transfers.createTransfer(transferPayload);
 
         // 4. Process the response
-        if (response.data && response.data.transfer) {
-            const transfer = response.data.transfer;
+        if (response.data && response.data.id) {
+            const transfer = response.data;
+            const transactionId = response.data.id;
             console.log("\n--- Transfer Initiated Successfully ---");
-            console.log("Transfer ID:", transfer.id);
-            console.log("Status:", transfer.status);
-            console.log("Source Wallet ID:", transfer.source.id);
-            console.log("Destination Address:", transfer.destination.address);
-            console.log("Expected Amount:", transfer.amount.amount, transfer.amount.currency);
+            console.log("Transaction ID:", transactionId);
+            await circleClient.getTransaction({"id":transactionId}).then(function(transaction){
+                console.log(transaction.data);
+                let transId = transaction.data.transaction.id;
+                console.log(transId.toString());
+                setInterval(getTransfer(transId.toString()), 5000); // Poll every 5 seconds
+            })
+            // console.log(transaction.data);
+            // let transId = transaction.data.transaction.id;
+            // console.log(transId);
+            // while(transfer.state==="INITIATED"){
+            //     console.log("...");
+            //     transaction = await circleClient.getTransaction({"id":transferId});
+            //     console.log(transfer.id);
+            //     console.log(transfer.state);
+            // }
+            // console.log("Status:", transfer.status);
+            // console.log("Source Wallet ID:", transfer.source.id);
+            // console.log("Destination Address:", transfer.destination.address);
+            // console.log("Expected Amount:", transfer.amount.amount, transfer.amount.currency);
             
             // Note: The transactionHash will only be available once the transfer status changes to 'complete'
             if (transfer.transactionHash) {
@@ -84,6 +99,22 @@ async function sendUsdcTransfer() {
         }
     }
 }
-
+async function getTransfer(id){
+    const circleClient = initiateDeveloperControlledWalletsClient({
+    apiKey:"TEST_API_KEY:a4c20122aeb5c8600299da79c3ba1a95:0bccb52ac8d8fc127abcaa067c1b61be" ,
+    entitySecret: "e262785a20bfc1edc87f8c41db906eb871256bb448184786621bd30e5d14a003",
+    // Use CircleEnvironments.sandbox for testing
+    // baseUrl: 'https://api.circle.com/v1/w3s', // default for production
+    });
+    let transaction = await circleClient.getTransaction({"id":id});
+    console.log(transaction.state);
+    if (transaction.state!="INITIATED"){
+        console.log("Status:", transaction.state);
+        console.log("Source Wallet ID:", transaction.source.id);
+        console.log("Destination Address:", transaction.destination.address);
+        console.log("Expected Amount:", transaction.amount.amount, transfer.amount.currency);
+    }
+    return true;
+}
 // Execute the function
 sendUsdcTransfer();
